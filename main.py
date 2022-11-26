fdd64bfac69b1958ed23df9d7fe74c31f1026898
iurii <kyw99@bk.ru>
2022-11-26 05:16:10 +0400

    add exception and update to pep-8

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import requests
 
+
 def main():
 
     url_template = "https://wttr.in/{}"
     locations = ["London", "SVO", "Cherepovets"]
-    payload = {"n" : "", "T" : "", "q" : "", "m" : "", "lang" : "ru"}
+    payload = {"n": "", "T": "", "q": "", "m": "", "lang": "ru"}
 
     for location in locations:
-        url = url_template.format(location)
-        response = requests.get(url, params=payload)
-        print(response.text)
+        try:
+            url = url_template.format(location)
+            response = requests.get(url, params=payload)
+            print(response.text)
+        except (requests.exceptions.ConnectionError):
+            print("Check your internet connection or accessibility of url.")
+
 
 if __name__ == '__main__':
     main()
